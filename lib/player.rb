47f1ec15d57e7f9e6451f283491213d08ff19bce
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-13 09:39:17 -0600

    feat: Add the player class and its methods to create readable attributes

---
+++ lib/player.rb
@@ -0,0 +1,25 @@
+class Player
+  attr_reader :full_name, :monthly_cost, :contract_length, :first_name
+  def initialize(full_name, monthly_cost, contract_length)
+    @full_name = full_name
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+    @first_name
+    @last_name
+  end
+
+  def first_name
+    array_of_full_name = full_name.split
+    first_name = array_of_full_name.first
+  end
+
+  def last_name
+    array_of_full_name = full_name.split
+    first_name = array_of_full_name.last
+  end
+
+  def total_cost
+    @monthly_cost * @contract_length
+  end
+  
+end
